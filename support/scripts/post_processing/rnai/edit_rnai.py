b8545e3f93451c8af3ab9e358c23633e7de96fa1
Paulo Nuin <nuin@genedrift.org>
2021-03-04 20:34:37 -0700

    more functions added to edit_rnai

--- support/scripts/post_processing/rnai/edit_rnai.py
+++ support/scripts/post_processing/rnai/edit_rnai.py
@@ -29,44 +29,45 @@ def remove_remarks():
             print('\n')
 
 
+def remove_secondaryidentifier():
+
+    sql_text = """SELECT * FROM rnai"""
+
+    res = db.execute(text(sql_text))
+    for row in res:
+        if str(row['secondaryidentifier']).find('CDATA') >= 0:
+            print(row['secondaryidentifier'])
+            secondaryidentifier = row['secondaryidentifier']
+            new_identifier = secondaryidentifier.replace('<![CDATA[', '').replace(']]>', '').replace('\'', '`')
+            print(new_identifier)
+            rnai_id = row['primaryidentifier']
+            print(rnai_id)
+            connection.execute("UPDATE rnai SET secondaryidentifier = '%s' where primaryidentifier = '%s'" %  (new_identifier, rnai_id))
+            print('updated ' + rnai_id)
+            print('\n')
+
+
+def remove_phenotyperemark():
+
+    sql_text = """SELECT * FROM rnai"""
+    
+    res = db.execute(text(sql_text))
+    for row in res:
+        if str(row['phenotyperemark']).find('CDATA') >= 0:
+            print(row['phenotyperemark'])
+            secondaryidentifier = row['phenotyperemark']
+            new_remark = phenotyperemark.replace('<![CDATA[', '').replace(']]>', '').replace('\'', '`')
+            print(new_remark)
+            rnai_id = row['primaryidentifier']
+            print(rnai_id)
+            connection.execute("UPDATE rnai SET phenotyperemark = '%s' where primaryidentifier = '%s'" %  (new_remark, rnai_id))
+            print('updated ' + rnai_id)
+            print('\n')
+
+
 if __name__ == '__main__':
 
-#    rnai_ids = open('to_remove_rnai.txt').read().splitlines()
-
     remove_remarks()
+    remove_secondaryidentifier()
+    remove_phenotyperemark()
 
-
-#     for i in rnai_ids:
-#         print(i)
-#         result = connection.execute("SELECT * from rnai where primaryidentifier = '%s'" % (i))
-#         for j in result:
-#             remark = j['remark']
-#             secondaryidentifier = j['secondaryidentifier']
-#             phenotyperemark = j['phenotyperemark']
-#             print(secondaryidentifier)
-# #            try:
-# #                new_remark = remark.replace('<![CDATA[', '').replace(']]>', '').replace('\'', '`')
-# #                print(new_remark)
-# #            except:
-# #                print('No remark')
-# #            try:
-# #                new_phenotyperemark = phenotyperemark.replace('<![CDATA[', '').replace(']]>', '').replace('\'', '`')
-# #                #print(new_phenotyperemark)
-# #            except Exception as e:
-# #                print(str(e))
-# #                print('failed')
-#             try:
-#                 new_identifier = secondaryidentifier.replace('<![CDATA[', '').replace(']]>', '').replace('\'', '`')
-#                 print(new_identifier)
-#             except:
-#                 print('no secondary')
-# #            try:
-# #            connection.execute("UPDATE rnai SET remark = '%s' where primaryidentifier = '%s'" %  (new_remark, i))
-#             connection.execute("UPDATE rnai SET secondaryidentifier = '%s' where primaryidentifier = '%s'" %  (new_identifier, i))
-# #            connection.execute("UPDATE rnai SET phenotyperemark = '%s' where primaryidentifier = '%s'" %  (new_phenotyperemark, i))
-# #            print("UPDATE rnai SET phenotyperemark = '%s' where primaryidentifier = '%s'" %  (new_phenotyperemark, i))
-# #                result = connection.execute("SELECT * from anatomyterm where primaryidentifier = '%s'" % (i))
-# #                for k in result:
-# #                    print(k['definition'])
-# #            except Exception as e:
-# #                print('error', str(e))
