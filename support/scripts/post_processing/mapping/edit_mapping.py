86db768d17f0b45da041c980c86d02eda1237db8
Paulo Nuin <nuin@genedrift.org>
2021-05-11 19:10:03 +0000

    tested version

--- support/scripts/post_processing/mapping/edit_mapping.py
+++ support/scripts/post_processing/mapping/edit_mapping.py
@@ -3,37 +3,47 @@
 import sys
 from sqlalchemy import create_engine
 
-#
-# db_string = "postgres://postgres:interwormmine@localhost/" + sys.argv[1] + "?client_encoding=utf8"
-# db = create_engine(db_string)
-# connection = db.connect()
+
+db_string = "postgres://postgres:interwormmine@localhost/" + sys.argv[1] + "?client_encoding=utf8"
+db = create_engine(db_string)
+connection = db.connect()
 
 def process_mapping(mapping):
 
-    mapping = ['IV', ':8.51401', '±0.001138']
-
     position, error = mapping[1], mapping[2]
 
-    position = position[0] + '{:0.2f}'.format(float(position[1:]))
-    error = error[0] + '{:0.2f}'.format(float(error[1:]))
+    position = position[0] + str('{:0.2f}'.format(float(position[1:])))
+    error = error[0] + str('{:0.2f}'.format(float(error[1:])))
 
-    print(position, error)
+    final_mapping = mapping[0] + position + ' '  + error[0] + ' ' + error[1:] + ' cM'
+
+    return final_mapping
 
 def get_mapping():
 
-    mappings = {}
-    all_genes = connection.execute('select * from gene where mapping is not null')
-    for row in all_genes:
-       temp = row['mapping'].split(' ')
-       to_add = [x for x in temp if x]
-       mappings[row['primaryidentifier']] = to_add
+    try:
+        mappings = {}
+        all_genes = connection.execute('select * from gene where mapping is not null')
+        for row in all_genes:
+           temp = row['mapping'].split(' ')
+           to_add = [x for x in temp if x]
+           mappings[row['primaryidentifier']] = process_mapping(to_add)
+    except:
+        pass
 
-    print(mappings)
+    return mappings
+
+def update_mappings(mappings):
+
+    for i in mappings:
+        print(i, mappings[i])
+        connection.execute("UPDATE gene SET mapping = '%s' where primaryidentifier = '%s'" % (mappings[i], i))
 
 if __name__ == '__main__':
 
     print('Reading mapping information from genes')
 
-    # get_mapping()
-    process_mapping('')
+    mappings = get_mapping()
+    update_mappings(mappings)
+#    process_mapping('')
 
