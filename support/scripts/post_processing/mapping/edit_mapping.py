06a5979fdc8afb7fe9268b073c0211ba4a937b3b
Paulo Nuin <nuin@genedrift.org>
2021-05-11 02:20:05 +0000

    second version

--- support/scripts/post_processing/mapping/edit_mapping.py
+++ support/scripts/post_processing/mapping/edit_mapping.py
@@ -4,16 +4,20 @@ import sys
 from sqlalchemy import create_engine
 
 
-db_string = "postgres://postgres:interwormmine@localhost/" + sys.argv[1]
+db_string = "postgres://postgres:interwormmine@localhost/" + sys.argv[1] + "?client_encoding=utf8"
 db = create_engine(db_string)
 connection = db.connect()
 
 
-
 if __name__ == '__main__':
-	
-	print('Reading mapping information from genes')
-	
-	all_genes = connection.execute('select * from gene where mapping is not null')
-	for row in all_genes:
-		print(row)
+
+    print('Reading mapping information from genes')
+
+    mappings = {}
+    all_genes = connection.execute('select * from gene where mapping is not null')
+    for row in all_genes:
+        temp = row['mapping'].split(' ')
+        to_add = [x for x in temp if x]
+        mappings[row['primaryidentifier']] = to_add
+ 
+    print(mappings)
