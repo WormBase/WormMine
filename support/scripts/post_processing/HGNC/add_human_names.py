4804525f6a8de47480bad57bbc8f2474d811ca0d
Paulo Nuin <nuin@genedrift.org>
2021-07-29 18:54:38 -0600

    HGNC script

--- support/scripts/post_processing/HGNC/add_human_names.py
+++ support/scripts/post_processing/HGNC/add_human_names.py
@@ -14,14 +14,13 @@ if __name__ == '__main__':
     print('starting')
     HGNC_ids = open('HGNC.txt').read().splitlines()
 
-    symbols = { x.split('\t')[0]: x.split('\t')[1] for x in  HGNC_ids}
- #   try:
-    human_query = connection.execute("SELECT * FROM gene WHERE primaryidentifier like '%%HGNC%%';")
-    for g in human_query:
-       print(g)
-
-#    for gene in human_query:
-#        print(gene['primaryidentifier'], symbols[gene['primaryidentifier']])
-#        connection.execute("UPDATE gene SET secondaryidentifier = '%s' where primaryidentifier = '%s'" % (symbols[gene['primaryidentifier']], gene['primaryidentifier']))
-#    except Exception as e:
-#        print(str(e))
+    symbols = {x.split('\t')[0]: x.split('\t')[1] for x in  HGNC_ids}
+    try:
+        human_query = connection.execute("SELECT * FROM gene WHERE primaryidentifier like '%%HGNC%%';")
+        for g in human_query:
+            print(g)
+        for gene in human_query:
+            print(gene['primaryidentifier'], symbols[gene['primaryidentifier']])
+            connection.execute("UPDATE gene SET secondaryidentifier = '%s' where primaryidentifier = '%s'" % (symbols[gene['primaryidentifier']], gene['primaryidentifier']))
+   except Exception as e:
+       print(str(e))
